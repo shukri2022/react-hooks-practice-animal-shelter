b5935f80c5bd28f62989b923d7a9a0e2a0cf3c57
Shukri Abdi <shukri751@gmail.com>
2024-08-24 03:23:06 +0300

    Code Challenges

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,41 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import Filters from './Filters';
+import PetBrowser from './PetBrowser';
+import data from '../db.json';
 
-import Filters from "./Filters";
-import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
-  const [filters, setFilters] = useState({ type: "all" });
+  const [filters, setFilters] = useState({ type: 'all' });
+
+  const handleTypeChange = (type) => {
+    setFilters({ ...filters, type });
+  };
+
+  const fetchPets = () => {
+    let filteredPets = data.pets;
+
+    if (filters.type !== 'all') {
+      filteredPets = data.pets.filter(pet => pet.type === filters.type);
+    }
+
+    setPets(filteredPets);
+  };
+
+  const handleAdoptPet = (id) => {
+    const updatedPets = pets.map(pet =>
+      pet.id === id ? { ...pet, isAdopted: true } : pet
+    );
+    setPets(updatedPets);
+  };
 
   return (
     <div className="ui container">
-      <header>
-        <h1 className="ui dividing header">React Animal Shelter</h1>
-      </header>
-      <div className="ui container">
-        <div className="ui grid">
-          <div className="four wide column">
-            <Filters />
-          </div>
-          <div className="twelve wide column">
-            <PetBrowser />
-          </div>
-        </div>
-      </div>
+      <Filters onChangeType={handleTypeChange} onFindPetsClick={fetchPets} />
+      <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
     </div>
   );
 }
 
 export default App;
+
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react';
 
-function Filters() {
+function Filters({ onChangeType, onFindPetsClick }) {
+  const handleTypeChange = (event) => {
+    onChangeType(event.target.value);
+  };
+
   return (
-    <div className="ui form">
-      <h3>Animal type</h3>
-      <div className="field">
-        <select name="type" id="type" aria-label="type">
-          <option value="all">All</option>
-          <option value="cat">Cats</option>
-          <option value="dog">Dogs</option>
-          <option value="micropig">Micropigs</option>
-        </select>
-      </div>
-
-      <div className="field">
-        <button className="ui secondary button">Find pets</button>
-      </div>
+    <div className="ui filter">
+      <select onChange={handleTypeChange} defaultValue="all">
+        <option value="all">All</option>
+        <option value="cat">Cats</option>
+        <option value="dog">Dogs</option>
+        <option value="micropig">Micropigs</option>
+      </select>
+      <button className="ui primary button" onClick={onFindPetsClick}>
+        Find pets
+      </button>
     </div>
   );
 }
